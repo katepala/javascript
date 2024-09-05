eb0656865d4f18dc039de856433613b624bdb619
Катерина <palam.kate@gmail.com>
2024-09-05 13:44:51 +0300

    newcommit

---
+++ hw6.js
@@ -0,0 +1,25 @@
+function checkProbabilityTheory(count){
+  let countEven = 0;
+  let countOdd = 0;
+  
+  for (let i = 0; i < count; i++) {
+  i= Math.floor(Math.random() * 1000 + 100); // шось тут не те ))
+
+    if (i % 2 === 0) {
+      countEven++;
+    } 
+    else {
+      countOdd++;
+    }
+  }
+  let evenPercentage = (countEven / count) * 100;
+  let oddPercentage = (countOdd / count) * 100;
+
+  console.log("Кількість згенерованих чисел: " + count);
+  console.log("Парних: " + countEven);
+  console.log("Непарних: " + countOdd);
+  console.log("Відсоток парних: " + evenPercentage + " %");
+  console.log("Відсоток непарних: " + oddPercentage + " %");
+  
+}
+checkProbabilityTheory(15);
