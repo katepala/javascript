627fff8d739e9f3056cee28d5687ab8c778140d9
katepala <55874742+katepala@users.noreply.github.com>
2024-09-21 19:39:56 +0300

    Add files via upload

---
+++ hw9.js
@@ -0,0 +1,61 @@
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+      {
+        name: 'Клієнт 1',
+        type: 'subCompany',
+        uses: 'ПО для продажу квитків',
+        sells: 'Рішення для продажу квитків',
+        partners: [
+          {
+            name: 'Клієнт 1.1',
+            type: 'subSubCompany',
+            uses: 'Рішення для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+          },
+          {
+            name: 'Клієнт 1.2',
+            type: 'subSubCompany',
+            uses: 'Рішення для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+              {
+                name: 'Клієнт 1.2.3',
+                type: 'subSubCompany',
+                uses: 'Рішення для продажу квитків',
+                sells: 'Рішення для продажу квитків',
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'Клієнт 2',
+        type: 'subCompany',
+        uses: 'ПО для продажу квитків',
+        sells: 'Рішення для продажу квитків'
+      }
+    ]  
+  };
+  
+  function findValueByKey(companyName) {
+    function searchClients(clients){
+      for(let client of clients){
+        if (client.name === companyName) {
+          console.log(Object.values(client));
+          return;
+        }
+        if(client.partners){
+          searchClients(client.partners);
+        }
+      }
+    }
+searchClients(company.clients);
+}
+  
+findValueByKey('Клієнт 1');
+  
+
